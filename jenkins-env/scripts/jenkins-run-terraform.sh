75dc75ce4c6c851db4fc8cd946f901cb60eb497e
dhong <dhong@fortinet.com>
2020-11-09 11:45:10 -0800

    terraform output

--- jenkins-env/scripts/jenkins-run-terraform.sh
+++ jenkins-env/scripts/jenkins-run-terraform.sh
@@ -13,3 +13,7 @@ aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
 terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
 
+sleep 30
+
+terraform output
+
